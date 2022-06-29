cb74feea71653d3ad33b97fc6714f06ed207270f
AsianCrouton <keithlow1@hotmail.com>
2022-06-29 00:14:52 -0700

    PostForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from "./logo.svg"
 import "./App.css"
+import PostForm from "./comp/PostForm"
+import { GoMegaphone } from "react-icons/go"
+
 
 function App() {
     return (
-        <div></div>
+        <div>
+            <div className="navbar bg-neutral text-2xl p-4 flex justify-start items-center shadow-md space-x-2">
+                <GoMegaphone size="28" className="hover:animate-pulse"/>
+                <span>SCREAMER</span>
+            </div>
+            <PostForm />
+            <footer className="footer items-center p-4 bg-neutral text-neutral-content">@Copyright?</footer>
+        </div>
     )
 }
 
---
+++ src/comp/PostForm.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const PostForm = () => {
+    return (
+        <div className="card space-y-2 p-2 m-2 flex">
+            <input className="input input-bordered input-info w-full max-w-xs" placeholder="WHO'S SCREAMING?" />
+            <input className="input input-bordered input-info w-full max-w-xs" placeholder="SCREAM HERE" />
+            <button className="btn w-48">Scream</button>
+        </div>
+    )
+}
+
+export default PostForm
