4dcb414e6b156329ad0d019486d70b0726236b92
AsianCrouton <keithlow1@hotmail.com>
2022-07-03 00:47:13 -0700

    changed search bar to filter

--- src/comp/PostDisplay.jsx
+++ src/comp/PostDisplay.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import PostItem from "./PostItem"
 import { useState } from "react"
 import { useSelector } from "react-redux/es/exports"
-import { useUpperField } from "../hooks"
 
 const PostDisplay = () => {
     const [sort, setSort] = useState("default")
@@ -14,16 +13,9 @@ const PostDisplay = () => {
         }
     }
 
-    const { clear: clearSearch, ...searchField } = useUpperField()
-    const [ search, setSearch ] = useState("")
+    const [filter, setFilter] = useState("")
 
-    const searchSubmitHandler = (e) => {
-        e.preventDefault()
-        setSearch(searchField.value)
-        clearSearch()
-    }
-
-    const posts = useSelector(({ posts }) => posts.filter(post => post.content.includes(search)).sort(sorter))
+    const posts = useSelector(({ posts }) => posts.filter(post => post.content.includes(filter)).sort(sorter))
 
     return (
         <div className="p-2 m-2 space-y-2 text-xl">
@@ -37,12 +29,7 @@ const PostDisplay = () => {
                 </div>
 
                 <div className="form-control flex-none">
-                    <form className="input-group" onSubmit={searchSubmitHandler}>
-                        <input {...searchField} type="text" placeholder="Search…" className="input input-bordered" />
-                        <button className="btn btn-square" type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                        </button>
-                    </form>
+                    <input value={filter} onChange={(e) => setFilter(e.target.value.toUpperCase())} type="text" placeholder="Filter..." className="input input-bordered" />
                 </div>
             </div>
             {posts.map(post => <PostItem key={post.id} post={post}/>)}
