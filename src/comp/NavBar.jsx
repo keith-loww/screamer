fe51334952bd1d8ee0ec3d890496278707ef4b99
AsianCrouton <keithlow1@hotmail.com>
2022-07-20 00:53:49 -0700

    glassed up header

--- src/comp/NavBar.jsx
+++ src/comp/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-base-200 sticky top-0 z-50 shadow-md">
+        <div className="navbar bg-None sticky top-0 z-50 shadow-md backdrop-blur backdrop-brightness-75">
             <Link to={"/"}>
                 <GoMegaphone size="32" className="hover:animate-pulse relative left-10"/>
                 <div className="btn btn-ghost normal-case text-xl relative left-10">SCREAMER</div>
