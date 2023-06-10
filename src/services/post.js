9ccdcd17961b77c4be6dc41e043831d676ed2638
keith-loww <keithlow1@hotmail.com>
2023-06-09 21:31:30 -0700

    correct end point

--- src/services/post.js
+++ src/services/post.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseUrl = "https://screamer-be.onrender.com/api/post"
+const baseUrl = "https://screamer-be.onrender.com/api/posts"
 
 const getAll = async () => {
     const resp = await axios.get(baseUrl)
